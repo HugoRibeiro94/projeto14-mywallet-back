706e1e9d443df22ab563b76a4557ea2db444648d
Hugo <hugoo.ribeiro03@gmail.com>
2023-08-28 10:41:43 -0300

    configuraçoes para deploy

--- src/app.js
+++ src/app.js
@@ -177,5 +177,5 @@ app.get("/", (request, response) => {
 })
 
 // rodando a aplicação servidora para ouvir requisições na porta 5000
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen( PORT, () => console.log(`Server running at port ${PORT}`))

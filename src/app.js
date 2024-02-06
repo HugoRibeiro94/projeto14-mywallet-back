bff82f74811e5d428709cb220374c56cbfc3f27c
Hugo <hugoo.ribeiro03@gmail.com>
2024-02-05 21:30:27 -0300

    fix: app.js

--- src/app.js
+++ src/app.js
@@ -3,16 +3,13 @@ import cors from "cors"
 import routerUser from "./routes/user.routes.js";
 import routerTransactions from "./routes/transactions.routes.js";
 
-// criando a aplicação servidora
 const app = express()
 
-// configurações
 app.use(cors())
 app.use(express.json())
 
 app.use(routerUser)
 app.use(routerTransactions)
 
-// rodando a aplicação servidora para ouvir requisições na porta 5000
 const PORT = process.env.PORT || 5000;
 app.listen( PORT, () => console.log(`Server running at port ${PORT}`))
